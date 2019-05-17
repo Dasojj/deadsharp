a9c9e0c4ee6cfa8f30cea7768ca5b85f4ab115cf
KirillM281 <KirillM28@yandex.ru>
2019-05-17 21:58:49 +0300

    Change directory(read description)
    Nice and simple GUI with to ways of changing directory(with the help of buttons and with the path writing)
    //proud of that one

--- deadsharp/deadsharp/Window1.xaml.cs
+++ deadsharp/deadsharp/Window1.xaml.cs
@@ -25,10 +25,13 @@ namespace deadsharp
     public partial class Window1 : Window
     {
         public string path = @"C:\Users\";
+        public string[] ars;
+        public int I = 0;
         public Window1()
         {
             InitializeComponent();
             windooow.Title = Exchange.s;
+            pathtb.Text = path;
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -36,6 +39,21 @@ namespace deadsharp
                 @"dir /b " + path + " > otv.txt");
             Process.Start("somescript.bat");
             dirinfo.Text = File.ReadAllText("otv.txt");
+            ars = File.ReadAllLines("otv.txt");
+            I = 0;
+            if (ars.Length != 0)
+            {
+                ch_cd.IsEnabled = true;
+                levo.IsEnabled = true;
+                pravo.IsEnabled = true;
+                ch_cd.Content = ars[I];
+            }
+            else
+            {
+                ch_cd.IsEnabled = false;
+                levo.IsEnabled = false;
+                pravo.IsEnabled = false;
+            }
         }
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
@@ -46,6 +64,38 @@ namespace deadsharp
                 mn1.Show();
                 this.Close();
             }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            pathtb.Text += "\\";
+            path = pathtb.Text;
+        }
+
+        private void Ch_cd_Click(object sender, RoutedEventArgs e)
+        {
+            path += (ch_cd.Content + "\\");
+            pathtb.Text = path;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (I > 0)
+            {
+                I--;
+                ch_cd.Content = ars[I];
+            }
+            else MessageBox.Show("It`s the 1st name in list");
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            if (I < ars.Length-1)
+            {
+                I++;
+                ch_cd.Content = ars[I];
+            }
+            else MessageBox.Show("It`s the last name in list");
         }
     }
 }
